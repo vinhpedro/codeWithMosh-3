8fe4fa5d57073693bfbbb7b3fb38242e26e54bb3
postfull <saeful.mahmud@gmail.com>
2019-12-31 21:20:37 +0700

    Attribute routing

--- CodeWithMosh/App_Start/RouteConfig.cs
+++ CodeWithMosh/App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace CodeWithMosh
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            /*
             routes.MapRoute(
                 "MoviesByReleaseDate",
                 "movies/released/{year}/{month}",
                 new { Controller = "Movies", action = "ByReleaseDate"},
                 new { year = @"\d{4}", month = @"\d{2}"});
+            */
 
             routes.MapRoute(
                 name: "Default",
--- CodeWithMosh/Controllers/MoviesController.cs
+++ CodeWithMosh/Controllers/MoviesController.cs
@@ -36,7 +36,8 @@ namespace CodeWithMosh.Controllers
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
-        public ActionResult ByReleaseDate(int year, int month)
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
         {
             return Content(year + "/" + month);
         }
