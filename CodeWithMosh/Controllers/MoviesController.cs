0ac5cb0486940f8534c50d0352fc31b4faf2f196
postfull <saeful.mahmud@gmail.com>
2019-12-31 19:54:49 +0700

    Action Parameters

--- CodeWithMosh/Controllers/MoviesController.cs
+++ CodeWithMosh/Controllers/MoviesController.cs
@@ -14,11 +14,26 @@ namespace CodeWithMosh.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            //return View(movie);
+            return View(movie);
             //return Content("Hellow World");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
