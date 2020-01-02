0084f32f332d484c8074f37e75b8eb42c39ea1ad
postfull <saeful.mahmud@gmail.com>
2020-01-02 14:12:22 +0700

    Exercise 2, i think this's not running, maybe iam back to this project to troubleshoot this ERROR

--- CodeWithMosh/Controllers/MoviesController.cs
+++ CodeWithMosh/Controllers/MoviesController.cs
@@ -36,14 +36,35 @@ namespace CodeWithMosh.Controllers
             return Content("id=" + id);
         }
 
+        //Replace for exercise 2
+        /*
         public ActionResult Index(int? pageIndex, string sortBy)
         {
+
             if (!pageIndex.HasValue)
                 pageIndex = 1;
             if (string.IsNullOrWhiteSpace(sortBy))
                 sortBy = "Name";
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+
+        }
+        */
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Frozeen"},
+                new Movie {Id = 2, Name = "Lalaland"}
+            };
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
